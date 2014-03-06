898cf405f157dca5d74641ba6cfd16e54a87b363
Audun Skau Hansen <audunsh@fv303north.(none)>
2014-03-06 12:40:46 +0100

    added to-do list in hfsolve.h

--- HartreeFock/testingHFSolve.h
+++ HartreeFock/testingHFSolve.h
@@ -16,7 +16,13 @@ int mainf(){
     string filename;
     filename = "m_elements_c.dat";
     // Make sure the filename is located in the build-folder !!!
-    // Hydrogen atom:
+    // Yet to be implemented (6.march 2014):
+    // (1) A more versatile class-structure
+    // (2) The Thijssen spin implementation (reducing the size of the matrix)
+    // (3) Gaussian Type Orbitals
+    // (4) If possible/sensible; a possibility to calculate excited states using excited SDs. (particle-hole states)
+    //     In effect this means to permute the C-matrix after each iteration, so that the two one-particle states is not the two lowest lying states.
+
     int Z = 2; // two protons
     int N = 2; // two electrons
     int Ns = 6; //6 states
