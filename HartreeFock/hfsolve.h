47d0bafb97c6340e64e86fb86b2d6cd1121d4d21
goranbs <goranbs@student.matnat.uio.no>
2014-03-17 10:01:29 +0100

    primitive, hfsolve merged

--- HartreeFock/hfsolve.h
+++ HartreeFock/hfsolve.h
@@ -15,16 +15,8 @@ public:
     void Solve(basis Bs);
 
 private:
-<<<<<<< HEAD
-    double h0(int alpha,int gamma);
-    mat HF(mat C, field<mat> V);
-    double calc_energy(mat C,field<mat> V);
-    void get_Q();
-    void get_S();
-=======
     mat HF(mat C, basis Bs);
     double calc_energy(mat C,basis Bs);
->>>>>>> 3b050e3940d186edb487b3e8edede6875e365eb8
 };
 
 #endif // HFSOLVE_H
