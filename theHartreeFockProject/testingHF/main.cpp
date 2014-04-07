6487d957e09b8cac53afc81217f330ce6f1483a5
goranbs <goranbs@student.matnat.uio.no>
2014-04-07 10:05:37 +0200

    Trying to fix the HFSolve nosetest

--- theHartreeFockProject/testingHF/main.cpp
+++ theHartreeFockProject/testingHF/main.cpp
@@ -23,8 +23,8 @@ TEST(HFSolve){
 
     basis Bs(3, 0);              //creating the basis object
     string filename;
-    filename = "m_elements_c.dat";
-    Bs.read("m_elements_c.dat", Z); //reading basis from file
+    filename = "/home/goranbs/goran/CompPhys/FYS4411 - CompPhys2/build-theHartreeFockProject-Desktop-Debug/testingHF/m_elements_c.dat";
+    Bs.read(filename, Z); //reading basis from file
     Bs.set_orthonormal(true);
 
     //Solving for N,Z with the provided basis
