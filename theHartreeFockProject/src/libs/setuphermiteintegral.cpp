f82b7ce3a7671ab2df6213d4d60c9ab56a92fd2f
goranbs <goranbs@student.matnat.uio.no>
2014-04-26 15:58:39 +0200

    done some more work on setupHermiteIntegral. Program running

--- theHartreeFockProject/src/libs/setuphermiteintegral.cpp
+++ theHartreeFockProject/src/libs/setuphermiteintegral.cpp
@@ -1,10 +1,25 @@
 #include "setuphermiteintegral.h"
 
-setupHermiteIntegral::setupHermiteIntegral(const double &p, const vec &Rpc){
+// Rpc = P-C.
+// p = a+b
+// t = i+j, u = k+l, v = m+n
+setupHermiteIntegral::setupHermiteIntegral(const double &p, const vec &Rpc, const int &t, const int &u, const int &v){
+    n = t+u+v;
+    double R = Rpc(0)*Rpc(0) + Rpc(1)*Rpc(1) +Rpc(2)*Rpc(2);
+    double Fn, R0;
+    BoysFunction F0(n);
+    F0.setx(p*R*R);
+    Fn = F0.returnValue(n);
+    R0 = pow((-2.0*p),(double) n)*Fn;
 
+    setupRtuv(R0,n,t,u,v);
 }
 
 field <mat> setupHermiteIntegral::ReturnHermiteIntegral(){
 
     return Rtuv;
+}
+
+void setupHermiteIntegral::setupRtuv(const double R0, const int n, const int t, const int u, const int v){
+    // Doing the recurcion, and set up Rtuv.
 }
--- theHartreeFockProject/src/libs/setuphermiteintegral.h
+++ theHartreeFockProject/src/libs/setuphermiteintegral.h
@@ -1,6 +1,7 @@
 #ifndef SETUPHERMITEINTEGRAL_H
 #define SETUPHERMITEINTEGRAL_H
 
+#include <boysfunction.h>
 #include <armadillo>
 
 using namespace arma;
@@ -8,11 +9,13 @@ using namespace arma;
 class setupHermiteIntegral
 {
 public:
-    setupHermiteIntegral(const double &p, const vec &Rpc);
+    setupHermiteIntegral(const double &p, const vec &Rpc, const int &t, const int &u, const int &v);
     field <mat> ReturnHermiteIntegral();
 
 private:
     field <mat> Rtuv;
+    void setupRtuv(const double R0, const int n, const int t, const int u, const int v);
+    int n;
 };
 
 #endif // SETUPHERMITEINTEGRAL_H
