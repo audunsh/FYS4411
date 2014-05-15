08b86b096eab52727d12d9dd2c4d125926f63017
goranbs <goranbs@student.matnat.uio.no>
2014-05-15 23:29:27 +0200

    PotentialHF.py working with generall directory refrences

--- python_programs/Potential_HF.py
+++ python_programs/Potential_HF.py
@@ -26,7 +26,14 @@ N = 2               # number of electrons orbiting a core
 Ns = 6              # number of single particle states
 #----------------------------------------------------------------------------
 
-os.environ["LD_LIBRARY_PATH"] = "/home/goranbs/goran/CompPhys/FYS4411 - CompPhys2/build-theHartreeFockProject-Desktop-Release/src/libs/"
+current_path =  os.path.realpath("Potential_HF.py")
+path_to_Release = os.path.abspath(os.path.join(current_path, "..", "..", "build-theHartreeFockProject-Desktop-Release"))
+
+path_to_HartreeFock = os.path.abspath(os.path.join(path_to_Release, "src", "HartreeFock"))
+path_to_libs = os.path.abspath(os.path.join(path_to_Release, "src", "libs"))
+
+os.environ["LD_LIBRARY_PATH"] = path_to_libs
+
 
 # Varyation of the core distances:
 R = np.zeros((n,1))
@@ -39,9 +46,9 @@ for i in range(len(R)):
     
     args = ["%d" % Z , "%d" % N , "%f" % R[i,0]]
     #print("args = ", args)
-    #E = sub.check_output(["/home/goranbs/goran/CompPhys/FYS4411 - CompPhys2/build-theHartreeFockProject-Desktop-Release/src/HartreeFock/HartreeFock", "%d"%Z, "%d"%N, "%f"%R[i,0]])
-    #En = sub.check_output(["/home/goranbs/goran/CompPhys/FYS4411 - CompPhys2/build-theHartreeFockProject-Desktop-Release/src/HartreeFock/HartreeFock", "%d" % Z , "%d" % N , "%f" % R[i,0]])
-    En = sub.check_output(["/home/goranbs/goran/CompPhys/FYS4411 - CompPhys2/build-theHartreeFockProject-Desktop-Release/src/HartreeFock/HartreeFock", args[0], args[1], args[2]])
+    
+    #En = sub.check_output(["/home/goranbs/goran/CompPhys/FYS4411 - CompPhys2/build-theHartreeFockProject-Desktop-Release/src/HartreeFock/HartreeFock", args[0], args[1], args[2]])
+    En = sub.check_output(["./HartreeFock", args[0], args[1], args[2]], cwd=path_to_HartreeFock)
     E[i] = float(En)    
     #print En
 
@@ -63,7 +70,7 @@ import matplotlib.pyplot as plt
 
 h = plt.figure()
 plt.plot(R,E,'b-*')
-plt.title('Potential distribution for He; Z=%g N=%g' % (Z,N))
+plt.title('Potential distribution for the Hydrogen atom)
 plt.legend('E(R)')
 plt.xlabel('R [a.u.]')
 plt.ylabel('Potential [a.u]')
